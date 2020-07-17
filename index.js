0d250ab5b0bf36b935eccf6017067a6fc107910a
PengfeiYuan <yuanpengfei@itiger.com>
2020-07-17 17:37:44 +0800

    update console content and add readme content

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const holdings = 5804.96;
 const saveCost = 15000 / holdings;
 
 function withPercent(num) {
-  return num.toFixed(2) + "%";
+  return "|" + num.toFixed(2) + "%|";
 }
 
 const level0 = 15500 / holdings;
@@ -13,13 +13,14 @@ const level3 = 18000 / holdings;
 const level4 = 19000 / holdings;
 const level5 = 20000 / holdings;
 console.log("curre:", 2.4014);
-console.log("本息之和", "卖出净值", "利润率");
-console.log("15000:", saveCost.toFixed(4), withPercent(0));
-console.log("15500:", level0.toFixed(4), withPercent(500 / 15000));
-console.log("16000:", level1.toFixed(4), withPercent(1000 / 15000));
-console.log("17000:", level2.toFixed(4), withPercent(2000 / 15000));
-console.log("18000:", level3.toFixed(4), withPercent(3000 / 15000));
-console.log("19000:", level4.toFixed(4), withPercent(4000 / 15000));
-console.log("20000:", level5.toFixed(4), withPercent(5000 / 15000));
+console.log("|本息之和", "|卖出净值", "|利润率|");
+console.log("|------|------|------|");
+console.log("|15000:|", saveCost.toFixed(4), withPercent(0));
+console.log("|15500:|", level0.toFixed(4), withPercent(500 / 15000));
+console.log("|16000:|", level1.toFixed(4), withPercent(1000 / 15000));
+console.log("|17000:|", level2.toFixed(4), withPercent(2000 / 15000));
+console.log("|18000:|", level3.toFixed(4), withPercent(3000 / 15000));
+console.log("|19000:|", level4.toFixed(4), withPercent(4000 / 15000));
+console.log("|20000:|", level5.toFixed(4), withPercent(5000 / 15000));
 
 // console.log(holdings - 5000 / 2.5711);
